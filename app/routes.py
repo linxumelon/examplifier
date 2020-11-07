30c64360535e08a9ebe89ad5d58cbe8c83a745ea
NancyQuris <liuyingnan1999@gmail.com>
2020-11-07 23:11:30 +0800

    update import package

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,12 @@
 import os
 import sys
-from flask_login import current_user, login_user, login_required
+from flask_login import current_user, login_user, login_required, logout_user
 from flask import render_template, flash, redirect, url_for, request, send_from_directory, Response
 from werkzeug.utils import secure_filename
 from werkzeug.urls import url_parse
 from app import app
 from app import db
-from app.forms import LoginForm, UploadTestFileForm, AddModuleForm
+from app.forms import LoginForm, UploadTestFileForm, AddModuleForm, RegistrationForm, IDENTITY_CHOICES
 from app.models import User, Module, Teaches, Takes, TestPaper, StudentSubmission
 from sqlalchemy import and_, or_, not_
 import cv2
@@ -17,8 +17,9 @@ BASEDIR = os.path.dirname(app.instance_path)
 
 @app.route('/')
 @app.route('/index')
-@login_required
 def index():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
     print("basedir = {}".format(BASEDIR))
     return render_template("index.html", title='Home Page')
 
@@ -126,6 +127,26 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(id=form.id.data, username=form.username.data, email=form.email.data, identity=dict(IDENTITY_CHOICES).get(form.identity.data))
+        user.set_password(form.password.data)
+        print("User has identity {}".format(user.identity))
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 def IsCurrentUserTeachingMod(modcode):
     teaches = Teaches.query.filter_by(modcode=modcode, instructor_id=current_user.id).first()
